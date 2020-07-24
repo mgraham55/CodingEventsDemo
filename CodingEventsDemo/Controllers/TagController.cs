64936d718588e93704376018a944a6be52a354c3
Ben Clark <brclark15@gmail.com>
2020-07-24 13:59:53 -0500

    Added Tag persistent Model class

---
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodingEventsDemo.Controllers
+{
+    public class TagController : Controller
+    {
+        private EventDbContext context;
+
+        public TagController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Tag> tags = context.Tags.ToList();
+            return View(tags);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddTagViewModel addTagViewModel = new AddTagViewModel();
+            return View(addTagViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ProcessCreateTagForm(AddTagViewModel addTagViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Tag newTag = new Tag
+                {
+                    Name = addTagViewModel.Name
+                };
+
+                context.Tags.Add(newTag);
+                context.SaveChanges();
+
+                return Redirect("/Tag");
+            }
+
+            return View("Create", addTagViewModel);
+        }
+    }
+}
---
+++ CodingEventsDemo/Models/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CodingEventsDemo.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Tag()
+        {
+        }
+
+        public Tag(string name)
+        {
+            Name = name;
+        }
+    }
+}
