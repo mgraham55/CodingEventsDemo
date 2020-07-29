75596ae33215a95c6fa82caf506c5f8ab5b20994
Ben Clark <brclark15@gmail.com>
2020-07-29 10:39:02 -0500

    Adding Tags and EventTags

--- CodingEventsDemo/Controllers/TagController.cs
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -53,5 +53,37 @@ namespace CodingEventsDemo.Controllers
 
             return View("Create", addTagViewModel);
         }
+
+        // Responds to URLS like /Tag/AddEvent/5
+        public IActionResult AddEvent(int id)
+        {
+            Event theEvent = context.Events.Find(id);
+            List<Tag> possibleTags = context.Tags.ToList();
+
+            AddEventTagViewModel viewModel = new AddEventTagViewModel(theEvent, possibleTags);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(AddEventTagViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                int eventId = viewModel.EventId;
+                int tagId = viewModel.TagId;
+
+                EventTag eventTag = new EventTag
+                {
+                    EventId = eventId,
+                    TagId = tagId
+                };
+                context.EventTags.Add(eventTag);
+                context.SaveChanges();
+
+                return Redirect("/Events/Detail/" + eventId);
+            }
+
+            return View(viewModel);
+        }
     }
 }
