585de4bd67fe7ff565f5efbfb7761d48bc71fded
Ben Clark <brclark15@gmail.com>
2020-07-31 12:18:58 -0500

    Add Profile Picture to Contact

--- CodingEventsDemo/Controllers/ContactController.cs
+++ CodingEventsDemo/Controllers/ContactController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
 using CodingEventsDemo.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,10 +16,12 @@ namespace CodingEventsDemo.Controllers
     public class ContactController : Controller
     {
         private EventDbContext context;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public ContactController(EventDbContext dbContext)
+        public ContactController(EventDbContext dbContext, IWebHostEnvironment hostEnvironment)
         {
             context = dbContext;
+            webHostEnvironment = hostEnvironment;
         }
 
         // GET /Contact
@@ -42,12 +46,15 @@ namespace CodingEventsDemo.Controllers
         {
             if (ModelState.IsValid)
             {
+                string uniqueFileName = _UploadedFile(viewModel);
+
                 Contact newContact = new Contact
                 {
                     Name = viewModel.FirstName + " " + viewModel.LastName,
                     Email = viewModel.Email,
                     PhoneNumber = viewModel.PhoneNumber,
-                    City = viewModel.City
+                    City = viewModel.City,
+                    ProfilePicture = uniqueFileName
                 };
 
                 context.Contacts.Add(newContact);
@@ -67,6 +74,25 @@ namespace CodingEventsDemo.Controllers
             ContactDetailViewModel viewModel = new ContactDetailViewModel(theContact);
 
             return View(viewModel);
+        }
+
+
+        private string _UploadedFile(AddContactViewModel viewModel)
+        {
+            string uniqueFileName = null;
+
+            if (viewModel.ProfileImage != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + viewModel.ProfileImage.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    viewModel.ProfileImage.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
         }
     }
 }
